7adbb91a21ed44f2a50c874d1f814614964da45e
Christine Brovkina <cbrowkina@hfk-bremen.de>
2022-09-11 09:34:49 +0200

    Manifest v2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,53 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+ var links = document.querySelectorAll('[id*="ergebnissForm:selectedSuchErgebnisFormTable:"] [id$=":6:fade"]')
+ let localStorage
+//console.log(links)
+var arraySet = (function() {
+    var executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+            localStorage['arrayCounter'] = links.length - 1
+            console.log('Set Storage'+  localStorage['arrayCounter'] )
+        }
+    };
+})();
+ window.addEventListener('load', (event) => {
 
-    // since only one tab should be active and in the current window at once
-    // the return variable should only have one entry
-    var activeTab = tabs[0];
-    var activeTabUrl = activeTab.url;
-    let doc =  fetchDocument(activeTabUrl)
-    console.log(doc)
+    console.log('page is fully loaded')
+    arraySet()
+    downloadXML()
+  });
 
- })
+//  window.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('DOM fully loaded and parsed');
+//     downloadXML()
+// });
+
+ function downloadXML() {
+    if (document.title.includes('Notice on costs')){
+       
+            console.log(document.title)
+            var downloadBtn = document.getElementById( 'form:kostenpflichtigabrufen' )
+            downloadBtn.click()
+            setTimeout(function(){
+                localStorage.setItem('arrayCounter') -= 1
+                console.log('Update Storage' + localStorage['arrayCounter'] )  
+            }, 2000)
+            window.history.back()    
+
+    } else if (document.title.includes('Search Result')) {
+        // links.forEach((element) => {
+        //      console.log(document.title)
+        //       element.click()
+        //     })
+            for (var i = localStorage.getItem('arrayCounter'); i >=0; i--) {
+               console.log(document.title)
+               links[i].click()
+            }
+        }
+
+    }
 
 
 
-function fetchDocument(url) {        
-    let response = fetch(url)
-    console.log(response)
-    let text = response.text()
-    if (!fetchDocument.parser) fetchDocument.parser = new DOMParser() // the dom parser is reusable
-    return fetchDocument.parser.parseFromString(text, "text/html")
-}
 
